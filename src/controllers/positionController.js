33ad59357ce46a3af0c3373a40e81b2d75c4583f
Petterson William <pettersonwilliam@Amigos-Mac-mini.local>
2022-11-10 16:45:06 -0300

    formatting

--- src/controllers/positionController.js
+++ src/controllers/positionController.js
@@ -1,83 +1,103 @@
 import BaseController from "./base";
 import PositionsService from "../services/PositionsService";
-import { pick } from 'lodash';
+import { pick } from "lodash";
 
 class PositionController extends BaseController {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.bindActions(['store','index','show','update','delete']);
-  }
+		this.bindActions(["store", "index", "show", "update", "delete"]);
+	}
 
-  async store(req, res) {
-    try {
-      const position = pick(req.data, ['name']);
+	async store(req, res) {
+		try {
+			const position = pick(req.data, ["name"]);
 
-      const { name } = await PositionsService.create(position)
+			const { name } = await PositionsService.create(position);
 
-      return this.handleResponse({ name } , res);
-    } catch (e) {
-      return this.handleError({
-        message: e.message
-      },req, res);
-    }
-  }
+			return this.handleResponse({ name }, res);
+		} catch (e) {
+			return this.handleError(
+				{
+					message: e.message,
+				},
+				req,
+				res
+			);
+		}
+	}
 
-  async index(req, res) {
-    try {
-      const position = await PositionsService.index();
+	async index(req, res) {
+		try {
+			const position = await PositionsService.index();
 
-      return this.handleResponse({ position } , res);
-    } catch (e) {
-      return this.handleError({
-        message:'Erro ao listar posição.'
-      },req, res);
-    }
-  }
+			return this.handleResponse({ position }, res);
+		} catch (e) {
+			return this.handleError(
+				{
+					message: "Erro ao listar posição.",
+				},
+				req,
+				res
+			);
+		}
+	}
 
-  async show(req, res) {
-    try {
-      const position = await PositionsService.show(req.filter.id);
+	async show(req, res) {
+		try {
+			const position = await PositionsService.show(req.filter.id);
 
-      return this.handleResponse({ position } , res);
-    } catch (e) {
-      return this.handleError({
-        message:'Erro ao listar posição.'
-      },req, res);
-    }
-  }
+			return this.handleResponse({ position }, res);
+		} catch (e) {
+			return this.handleError(
+				{
+					message: "Erro ao listar posição.",
+				},
+				req,
+				res
+			);
+		}
+	}
 
-  async update(req, res) {
-    try {
-      const filter =  pick(req.filter, ['id']);
-      const changes =  pick(req.data, ['name', 'email', 'password']);
+	async update(req, res) {
+		try {
+			const filter = pick(req.filter, ["id"]);
+			const changes = pick(req.data, ["name", "email", "password"]);
 
-      const position = await PositionsService.update(filter, changes);
+			const position = await PositionsService.update(filter, changes);
 
-      return this.handleResponse({ position } , res);
-    } catch (e) {
-      return this.handleError({
-        message:'Erro ao atualizar posição.'
-      },req, res);
-    }
-  }
+			return this.handleResponse({ position }, res);
+		} catch (e) {
+			return this.handleError(
+				{
+					message: "Erro ao atualizar posição.",
+				},
+				req,
+				res
+			);
+		}
+	}
 
-  async delete(req, res) {
-    try {
-      const { id } = req.filter;
+	async delete(req, res) {
+		try {
+			const { id } = req.filter;
 
-      if (!id) {
-        return res.json("ID não existe");
-      }
+			if (!id) {
+				return res.json("ID não existe");
+			}
 
-      await PositionsService.delete(id);
+			await PositionsService.delete(id);
 
-      return this.handleResponse( true , res);
-    } catch (e) {
-      return this.handleError({
-        message:'Erro ao deletar posição.'
-      },req, res);
-    }
-  }
+			return this.handleResponse(true, res);
+		} catch (e) {
+			return this.handleError(
+				{
+					message: "Erro ao deletar posição.",
+				},
+				req,
+				res
+			);
+		}
+	}
 }
 export default PositionController;
--- src/services/PositionsService.js
+++ src/services/PositionsService.js
@@ -1,58 +1,57 @@
-import Position from '../models/Position';
+import Position from "../models/Position";
 
 class PositionsService {
-    async create(position) {
+	async create(position) {
 		const ecxistentPosition = await Position.findOne({
 			where: {
 				name: position.name,
-				deleted_at: null
+				deleted_at: null,
 			},
-			paranoid: false
+			paranoid: false,
 		});
 
-		if(ecxistentPosition) {
-			throw new Error('Erro! A posição já existe');
+		if (ecxistentPosition) {
+			throw new Error("Erro! A posição já existe");
 		}
-        return Position.create(position)
-    }
+		return Position.create(position);
+	}
 
-    index() {
-        return Position.findAll({ attributes: ['id', 'name'] });
-    }
+	index() {
+		return Position.findAll({ attributes: ["id", "name"] });
+	}
 
-    async show(id) {
-        const position = await Position.findOne({
-            where: {
-                id,
-                deleted_at: null
-            },
-            paranoid: false,
-            attributes: [ 'id', 'name']
-        });
+	async show(id) {
+		const position = await Position.findOne({
+			where: {
+				id,
+				deleted_at: null,
+			},
+			paranoid: false,
+			attributes: ["id", "name"],
+		});
 
-        if (!position) {
-            throw new Error('posição não existe');
-        }
-            return position;
-    }
+		if (!position) {
+			throw new Error("posição não existe");
+		}
+		return position;
+	}
 
-    async update(filter, changes)  {
-        return Position.update(changes, {
-            where: {
-                id: filter.id
-            }
-        });
-    }
+	async update(filter, changes) {
+		return Position.update(changes, {
+			where: {
+				id: filter.id,
+			},
+		});
+	}
 
-    async delete(id) {
-        await Position.destroy({
-            where: {
-                id,
-                deleted_at: null
-            },
-            paranoid: false
-        });
-    }
-
+	async delete(id) {
+		await Position.destroy({
+			where: {
+				id,
+				deleted_at: null,
+			},
+			paranoid: false,
+		});
+	}
 }
 export default new PositionsService();
